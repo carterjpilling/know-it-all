4519f8f5a34f454362908d275061470c39417220
Cj <carterjpilling@gmail.com>
2020-09-25 16:43:17 -0600

    Added authReducer and Store, Updated Index

---
+++ src/reducks/authReducer.js
@@ -0,0 +1,53 @@
+// import axios from ' axios'
+
+const initialState = {
+  username: '',
+  email: '',
+  profile_pic: '',
+  points: null,
+  title: ''
+}
+
+const LOGIN_USER = 'LOGIN_USER'
+const LOGOUT_USER = 'LOGOUT_USER'
+const GET_USER = 'GET_USER'
+
+export function loginUser(user) {
+  return {
+    type: LOGIN_USER,
+    payload: user
+  }
+}
+
+export function logoutUser() {
+  return {
+    type: LOGOUT_USER,
+    payload: null
+  }
+}
+
+// export function getUser() {
+//   const payload = axios.get('/api/auth/me')
+
+//   return {
+//     type: GET_USER,
+//     payload: payload
+//   }
+// }
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case LOGIN_USER:
+      return { ...state, user: action.payload }
+    case LOGOUT_USER:
+      return initialState
+    case GET_USER + '_PENDING':
+      return { ...state }
+    case GET_USER + '_FULFILLED':
+      return { ...state, user: action.payload.data }
+    case GET_USER + '_REJECTED':
+      return initialState
+    default:
+      return state
+  }
+}
