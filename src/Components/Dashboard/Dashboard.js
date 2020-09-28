4a6d4cc78a77e451204c459a82471e1c88e2e049
Cj <carterjpilling@gmail.com>
2020-09-28 17:31:40 -0600

    State can update on Game

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,23 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import Game from '../Game/Game'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import axios from 'axios'
 
 function Dashboard() {
 
-  const [category, setCategory] = useState({
-    title: false,
-    artist: false,
-    date: false
-  })
 
-  useEffect(() => {
-    setCategory({
-      title: false,
-      artist: false,
-      date: false
-    })
-  }, [])
+
+
 
 
 
@@ -26,12 +14,12 @@ function Dashboard() {
   return (
     <div>
       {/* <Game chosenCategory={category} /> */}
-      <Link to={'/game/european'}>MET European Art On Display - Title</Link>
-      <Link to={'/game/european'}> MET European Art On Display - Artist</Link>
-      <Link to={'/game/displayed_art'}> MET Displayed Art - Date</Link>
-      <Link to={'/game/permanent_paintings'}> MET Permanent Painting Collection on Display - Artist</Link>
-      <Link to={'/game/permanent_paintings'}> MET Permanent Painting Collection on Display - Title</Link>
-      <Link to={'/game/american'}> MET American Collection - Title</Link>
+      <Link to={'/game/european/title'}>MET European Art On Display - Title</Link>
+      <Link to={'/game/european/artist'}> MET European Art On Display - Artist</Link>
+      <Link to={'/game/displayed_art/date'}> MET Displayed Art - Date</Link>
+      <Link to={'/game/permanent_paintings/artist'}> MET Permanent Painting Collection on Display - Artist</Link>
+      <Link to={'/game/permanent_paintings/title'}> MET Permanent Painting Collection on Display - Title</Link>
+      <Link to={'/game/american/title'}> MET American Collection - Title</Link>
     </div>
   )
 }
--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -4,12 +4,44 @@ import { withRouter } from 'react-router-dom'
 
 function Game(props) {
 
+  const [category, setCategory] = useState({
+    title: false,
+    artist: false,
+    date: false,
+    gameArray: [],
+    isLoading: true
+  })
+
+
+
+
   useEffect(() => {
-    axios.get(`/api/art/${props.match.params.category}`)
+    console.log(props.match.params.type)
+    axios.get(`/api/art/${props.match.params.category}`).then((res) => {
+      console.log(res.data)
+      setCategory((prevState) => {
+        return { ...prevState, [props.match.params.type]: true, gameArray: res.data, isLoading: false }
+      })
+    })
+    // console.log(category)
   }, [])
+  // console.log(category)
+  if (category.isLoading ? true : console.log(category.gameArray[0][0].primaryImage)) {
+
+  }
+  // const arrayIndex = category.gameArray[0]
+  // console.log(arrayIndex)
+  // console.log(category.gameArray[0][0])
+  // console.log(category.gameArray[0][0].primaryImage)
+
+
   return (
     <div>
-      Game.js
+      {category.isLoading === true && (
+        <p>Loading...</p>
+      )}
+        Game.js
+      {/* <img src={category.gameArray[0][0].primaryImage} alt='reallycoolpicture' /> */}
     </div>
   )
 }
