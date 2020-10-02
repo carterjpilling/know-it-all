7524eab0285e58b8a879c56fdb264e9c2f286919
Cj <carterjpilling@gmail.com>
2020-10-01 18:59:44 -0600

    Chart is up

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import axios from 'axios'
+import { ResponsiveBar } from '@nivo/bar'
+import './Statistics.css'
 
 
 function Statistics(props) {
@@ -348,7 +350,97 @@ function Statistics(props) {
 
   if (allStats.isLoading === true) {
     return (
-      <div>
+      <div className='statistics-container'>
+        <ResponsiveBar data={databank.dataArr}
+          keys={['date', 'artist', 'title']}
+          indexBy="category"
+          margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+          padding={0.3}
+          groupMode="grouped"
+          colors={{ scheme: 'nivo' }}
+          defs={[
+            {
+              id: 'dots',
+              type: 'patternDots',
+              background: 'inherit',
+              color: '#38bcb2',
+              size: 4,
+              padding: 1,
+              stagger: true
+            },
+            {
+              id: 'lines',
+              type: 'patternLines',
+              background: 'inherit',
+              color: '#eed312',
+              rotation: -45,
+              lineWidth: 6,
+              spacing: 10
+            }
+          ]}
+          fill={[
+            {
+              match: {
+                id: 'fries'
+              },
+              id: 'dots'
+            },
+            {
+              match: {
+                id: 'sandwich'
+              },
+              id: 'lines'
+            }
+          ]}
+          borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'country',
+            legendPosition: 'middle',
+            legendOffset: 32
+          }}
+          axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'average score',
+            legendPosition: 'middle',
+            legendOffset: -40
+          }}
+          labelSkipWidth={12}
+          labelSkipHeight={12}
+          labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+          legends={[
+            {
+              dataFrom: 'keys',
+              anchor: 'bottom-right',
+              direction: 'column',
+              justify: false,
+              translateX: 120,
+              translateY: 0,
+              itemsSpacing: 2,
+              itemWidth: 100,
+              itemHeight: 20,
+              itemDirection: 'left-to-right',
+              itemOpacity: 0.85,
+              symbolSize: 20,
+              effects: [
+                {
+                  on: 'hover',
+                  style: {
+                    itemOpacity: 1
+                  }
+                }
+              ]
+            }
+          ]}
+          animate={true}
+          motionStiffness={90}
+          motionDamping={15} />
       </div>
     )
   } else {
