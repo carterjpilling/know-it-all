749922b8a483e122de98e2f3225c36982fddfc0b
Cj <carterjpilling@gmail.com>
2020-10-08 13:25:10 -0600

    Game categories connectecd to isLoggedIn

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 import skullImage from '../../assets/skull.jpg'
 import americanImage from '../../assets/american.jpg'
 import danceImage from '../../assets/dance.jpg'
 
 
-function Dashboard() {
-
+function Dashboard(props) {
   return (
     <div className='dashboard-container'>
       <div className='art-bar'>
@@ -14,29 +14,42 @@ function Dashboard() {
       </div>
       <div className='game-container'>
         <div className='picture-box-container'>
-          <Link to='/game/european'>
-            <div className='image-wrapper' >
-              <img className='category-picture' src={skullImage} alt='Renoir Painting' />
-            </div>
-          </Link>
+          {props.isLoggedIn === true ?
+            <Link to='/game/european' >
+              <div className='image-wrapper' >
+                <img className='category-picture' src={skullImage} alt='Renoir Painting' />
+              </div>
+            </Link>
+            :
+            <div className='fake-image-wrapper' >
+              <img className='category-picture' src={skullImage} alt='Renoir Painting' /> </div>}
           <h2>European Art</h2>
         </div >
         <div className='picture-box-container'>
-          <Link to='/game/permanent'>
-            <div className='image-wrapper' >
+          {props.isLoggedIn === true ?
+            <Link to='/game/permanent'>
+              <div className='image-wrapper' >
+                <img className='category-picture' src={danceImage} alt='Dancers Painting' />
+              </div>
+            </Link>
+            : <div className='fake-image-wrapper' >
               <img className='category-picture' src={danceImage} alt='Dancers Painting' />
             </div>
-          </Link>
+          }
           <div>
             <h2>Paintings</h2>
           </div>
         </div>
         <div className='picture-box-container'>
-          <Link to='/game/american'>
-            <div className='image-wrapper'>
+          {props.isLoggedIn === true ?
+            <Link to='/game/american'>
+              <div className='image-wrapper'>
+                <img className='category-picture' src={americanImage} alt='American Art Painting' />
+              </div>
+            </Link>
+            : <div className='fake-image-wrapper'>
               <img className='category-picture' src={americanImage} alt='American Art Painting' />
-            </div>
-          </Link>
+            </div>}
           <h2>American Art</h2>
         </div>
       </div>
@@ -48,4 +61,6 @@ function Dashboard() {
   )
 }
 
-export default (Dashboard)
+const mapStateToProps = (reduxState) => reduxState
+
+export default connect(mapStateToProps)(Dashboard)
